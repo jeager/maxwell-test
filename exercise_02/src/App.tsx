688ea6c4ae9f46b9be330bbf3b8062ecf8bede1a
Vinicius Franco <viniciusf.arantes@gmail.com>
2021-10-08 08:15:24 -0300

    feat: adds calculator to App.jsx

--- exercise_02/src/App.tsx
+++ exercise_02/src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
+import Calculator from "./containers/Calculator";
+import { TProduct } from "./type";
+
+const products: TProduct[] = [
+  { name: "milk", price: 3.97, sale: { quantity: 2, price: 5 } },
+  { name: "bread", price: 2.17, sale: { quantity: 3, price: 6 } },
+  { name: "banana", price: 0.99 },
+  { name: "apple", price: 0.89 },
+];
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Calculator products={products} />
+    </div>
+  );
 }
 
 export default App;
